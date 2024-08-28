22d60f23676bd8c6bc0a881ec6e71543ac2f1127
Medido1 <medi1222@gmail.com>
2024-08-28 15:38:25 +0100

    Add custom checkbox

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const form = document.querySelector(".form");
 const sucessStateContainer = document.querySelector(".sucess_state");
+const checkBoxInput = document.querySelector(".checkbox_input");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   sucessStateContainer.classList.remove("hidden");
 })
+
