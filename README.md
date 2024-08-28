c070dad02fd5b4e62a8eb49aca4b6915d5f36c88
Medido1 <medi1222@gmail.com>
2024-08-28 18:04:40 +0100

    Completed Challenge

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# Frontend-Mentor-Contact-form
-This is a solution to Contact form challenge on Frontend Mentor
+# Frontend Mentor - Contact form solution
+
+This is a solution to the [Contact form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/contact-form--G-hYlqKJj).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Complete the form and see a success toast message upon successful submission
+- Receive form validation messages if:
+  - A required field has been missed
+  - The email address is not formatted correctly
+- Complete the form only using their keyboard
+- Have inputs, error messages, and the success message announced on their screen reader
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+# Desktop
+![](./sceenShots/desktopss.png)
+# Error handleing
+![](./sceenShots/errorHandanding.png)
+# Completed Form
+![](./sceenShots//completedForm.png)
+# Mobile
+![](./sceenShots/mobileSS.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://github.com/Medido1/Frontend-Mentor-Contact-form)
+- Live Site URL: [Add live site URL here](https://medido1.github.io/Frontend-Mentor-Contact-form/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [Medido1](https://www.frontendmentor.io/profile/Medido1)
+- Github - [Medido1](https://github.com/Medido1)
--- script.js
+++ script.js
@@ -1,9 +1,97 @@
 const form = document.querySelector(".form");
 const sucessStateContainer = document.querySelector(".sucess_state");
 const checkBoxInput = document.querySelector(".checkbox_input");
+const inputs = document.querySelectorAll(".input_grp input");
+const errorMsgs = document.querySelectorAll(".error_msg");
+const radioBtnInputs = document.querySelectorAll("input[type='radio']");
+const radioErrMsg = document.querySelector("fieldset .error_msg");
+const textArea = document.querySelector("textarea");
+const textAreaError = document.querySelector(".message_container .error_msg");
+const consentCheck = document.querySelector("#consent");
+const consentError = document.querySelector(".checkbox_input .error_msg");
+
+function isValidEmail(email){
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function showError(index, errorMsg){
+  errorMsgs[index].classList.remove("hidden");
+  errorMsgs[index].textContent = errorMsg;
+}
+
+function removeError(index) {
+  errorMsgs[index].classList.add("hidden");
+}
+
+function showRadioError() {
+  radioErrMsg.classList.remove("hidden");
+}
+
+function removeRadioError() {
+  radioErrMsg.classList.add("hidden");
+}
+
+function showTextAreaError() {
+  textAreaError.classList.remove("hidden");
+}
+
+function removeTextAreaError() {
+  textAreaError.classList.add("hidden");
+}
+
+function showConsentError(){
+  consentError.classList.remove("hidden");
+}
+
+function removeConsentError() {
+  consentError.classList.add("hidden");
+}
+
+function checkForError() {
+  let errorFound = false;
+  inputs.forEach((input, index) => {
+    if (input.value.trim() === ""){
+      let errorMsg = "this field is required";
+      showError(index, errorMsg);
+      errorFound = true;
+    } else if (input.id === "email" && !isValidEmail(input.value)){
+      let errorMsg = "please enter a valid email adress"
+      showError(index, errorMsg)
+      errorFound = true;
+    }
+  })
+  let radioChecked = [...radioBtnInputs].some(input => input.checked);
+  if (!radioChecked) {
+    showRadioError();
+    errorFound = true;
+  }
+  if (textArea.value.trim() === ""){
+    showTextAreaError();
+    errorFound = true;
+  }
+  if (!consentCheck.checked) {
+    showConsentError();
+    errorFound = true;
+  }
+  return errorFound;
+}
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  sucessStateContainer.classList.remove("hidden");
+  if (!checkForError()){
+    sucessStateContainer.classList.remove("hidden");
+    form.reset();
+  }
 })
+
+inputs.forEach((input, index) => {
+  input.addEventListener("focus", () => removeError(index))
+})
+radioBtnInputs.forEach((input) => {
+  input.addEventListener("click", removeRadioError);
+})
+textArea.addEventListener("focus", removeTextAreaError);
+consentCheck.addEventListener("click", removeConsentError);
 
